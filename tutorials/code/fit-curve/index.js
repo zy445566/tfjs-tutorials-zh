93f05a60985599b25cd5dcbf301a16cf4f4feffc
zy445566 <zycy@vip.qq.com>
2018-11-19 22:17:54 +0800

    add my fit curve

--- tutorials/code/fit-curve/index.js
+++ tutorials/code/fit-curve/index.js
@@ -1,152 +1,92 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
+const tf = require('@tensorflow/tfjs');
 
-import * as tf from '@tensorflow/tfjs';
-import {generateData} from './data';
-import {plotData, plotDataAndPredictions, renderCoefficients} from './ui';
-
-/**
- * We want to learn the coefficients that give correct solutions to the
- * following cubic equation:
- *      y = a * x^3 + b * x^2 + c * x + d
- * In other words we want to learn values for:
- *      a
- *      b
- *      c
- *      d
- * Such that this function produces 'desired outputs' for y when provided
- * with x. We will provide some examples of 'xs' and 'ys' to allow this model
- * to learn what we mean by desired outputs and then use it to produce new
- * values of y that fit the curve implied by our example.
- */
-
-// Step 1. Set up variables, these are the things we want the model
-// to learn in order to do prediction accurately. We will initialize
-// them with random values.
+// 初始化变量a,b,c,d
 const a = tf.variable(tf.scalar(Math.random()));
 const b = tf.variable(tf.scalar(Math.random()));
 const c = tf.variable(tf.scalar(Math.random()));
 const d = tf.variable(tf.scalar(Math.random()));
 
-
-// Step 2. Create an optimizer, we will use this later. You can play
-// with some of these values to see how the model performs.
-const numIterations = 75;
-const learningRate = 0.5;
-const optimizer = tf.train.sgd(learningRate);
-
-// Step 3. Write our training process functions.
-
-/*
- * This function represents our 'model'. Given an input 'x' it will try and
- * predict the appropriate output 'y'.
- *
- * It is also sometimes referred to as the 'forward' step of our training
- * process. Though we will use the same function for predictions later.
- *
- * @return number predicted y value
- */
+// 构建算法的模型
 function predict(x) {
-  // y = a * x ^ 3 + b * x ^ 2 + c * x + d
-  return tf.tidy(() => {
-    return a.mul(x.pow(tf.scalar(3, 'int32')))
-      .add(b.mul(x.square()))
-      .add(c.mul(x))
-      .add(d);
-  });
-}
-
-/*
- * This will tell us how good the 'prediction' is given what we actually
- * expected.
- *
- * prediction is a tensor with our predicted y values.
- * labels is a tensor with the y values the model should have predicted.
- */
-function loss(prediction, labels) {
-  // Having a good error function is key for training a machine learning model
-  const error = prediction.sub(labels).square().mean();
-  return error;
-}
-
-/*
- * This will iteratively train our model.
- *
- * xs - training data x values
- * ys — training data y values
- */
-async function train(xs, ys, numIterations) {
-  for (let iter = 0; iter < numIterations; iter++) {
-    // optimizer.minimize is where the training happens.
-
-    // The function it takes must return a numerical estimate (i.e. loss)
-    // of how well we are doing using the current state of
-    // the variables we created at the start.
-
-    // This optimizer does the 'backward' step of our training process
-    // updating variables defined previously in order to minimize the
-    // loss.
-    optimizer.minimize(() => {
-      // Feed the examples into the model
-      const pred = predict(xs);
-      return loss(pred, ys);
+    // y = a * x ^ 3 + b * x ^ 2 + c * x + d
+    return tf.tidy(() => {
+        return a.mul(x.pow(tf.scalar(3))) // a * x^3
+        .add(b.mul(x.square())) // + b * x ^ 2
+        .add(c.mul(x)) // + c * x
+        .add(d); // + d
     });
-
-    // Use tf.nextFrame to not block the browser.
-    await tf.nextFrame();
-  }
 }
 
-async function learnCoefficients() {
-  const trueCoefficients = {a: -.8, b: -.2, c: .9, d: .5};
-  const trainingData = generateData(100, trueCoefficients);
-
-  // Plot original data
-  renderCoefficients('#data .coeff', trueCoefficients);
-  await plotData('#data .plot', trainingData.xs, trainingData.ys)
-
-  // See what the predictions look like with random coefficients
-  renderCoefficients('#random .coeff', {
-    a: a.dataSync()[0],
-    b: b.dataSync()[0],
-    c: c.dataSync()[0],
-    d: d.dataSync()[0],
-  });
-  const predictionsBefore = predict(trainingData.xs);
-  await plotDataAndPredictions(
-      '#random .plot', trainingData.xs, trainingData.ys, predictionsBefore);
-
-  // Train the model!
-  await train(trainingData.xs, trainingData.ys, numIterations);
-
-  // See what the final results predictions are after training.
-  renderCoefficients('#trained .coeff', {
-    a: a.dataSync()[0],
-    b: b.dataSync()[0],
-    c: c.dataSync()[0],
-    d: d.dataSync()[0],
-  });
-  const predictionsAfter = predict(trainingData.xs);
-  await plotDataAndPredictions(
-      '#trained .plot', trainingData.xs, trainingData.ys, predictionsAfter);
-
-  predictionsBefore.dispose();
-  predictionsAfter.dispose();
+// 定义损耗函数，使用MSE(均方误差)来检测误差
+function loss(predictions, labels) {
+    // Subtract our labels (actual values) from predictions, square the results,
+    // and take the mean.
+    const meanSquareError = predictions.sub(labels).square().mean();
+    return meanSquareError;
 }
 
+// 定义优化器
+function optimizer(predictions, labels) {
+    const learningRate = 0.5;
+    return tf.train.sgd(learningRate);
+}
 
-learnCoefficients();
+// 定义循环训练
+function train(xs, ys, numIterations = 75) {
+    const optimize = optimizer();
+
+    for (let iter = 0; iter < numIterations; iter++) {
+        optimize.minimize(() => {
+        const predsYs = predict(xs);
+        return loss(predsYs, ys);
+        });
+    }
+}
+
+// 生成数据函数
+function generateData(numPoints, coeff, sigma = 0.04) {
+    return tf.tidy(() => {
+      const [a, b, c, d] = [
+        tf.scalar(coeff.a), tf.scalar(coeff.b), tf.scalar(coeff.c),
+        tf.scalar(coeff.d)
+      ];
+  
+      const xs = tf.randomUniform([numPoints], -1, 1);
+  
+      // Generate polynomial data
+      const three = tf.scalar(3, 'int32');
+      const ys = a.mul(xs.pow(three))
+        .add(b.mul(xs.square()))
+        .add(c.mul(xs))
+        .add(d)
+        // Add random noise to the generated data
+        // to make the problem a bit more interesting
+        .add(tf.randomNormal([numPoints], 0, sigma));
+  
+      // Normalize the y values to the range 0 to 1.
+      const ymin = ys.min();
+      const ymax = ys.max();
+      const yrange = ymax.sub(ymin);
+      const ysNormalized = ys.sub(ymin).div(yrange);
+  
+      return {
+        xs, 
+        ys: ysNormalized
+      };
+    })
+}
+
+const trueCoefficients = {a: -.8, b: -.2, c: .9, d: .5};
+const trainingData = generateData(100, trueCoefficients);
+console.log("---True a,b,c,d:-0.800,-0.200,0.900,0.500")
+console.log("---train Before a,b,c,d:")
+a.print()
+b.print()
+c.print()
+d.print()
+train(trainingData.xs, trainingData.ys,1000);
+console.log("---train After a,b,c,d:")
+a.print()
+b.print()
+c.print()
+d.print()
