45a1dd2de2088d848195614781bcb279697d31ca
ZhangYuan <zyct@vip.qq.com>
2018-11-21 10:38:01 +0800

    more true

--- tutorials/code/mnist/data.js
+++ tutorials/code/mnist/data.js
@@ -133,7 +133,7 @@ class MnistDataset {
     // Only create one big array to hold batch of images.
     // 一组图像为什么是4维？
     // 第一个变量有多少张图
-    // 第二和第三分别代表高和宽[其中高宽维度存储颜色]
+    // 第二和第三分别代表高和宽[其中高宽维度存储点位]
     // 第四个变量表示一个以上属于一个维度
     const imagesShape = [size, IMAGE_HEIGHT, IMAGE_WIDTH, 1];
     const images = new Float32Array(tf.util.sizeFromShape(imagesShape));
